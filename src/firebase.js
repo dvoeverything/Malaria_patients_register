23b2b5a0294d29c1f73ffe2a37c6900769d35a31
R_USSIA <dvoch5@gmail.com>
2022-03-30 21:37:54 +0200

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -9,12 +9,12 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDu8fQU6MgaUmdg9msYFfs1xS7fwzLnmAU",
-  authDomain: "malaria-patient-register.firebaseapp.com",
+  apiKey:"",//add from firebase
+  authDomain: "",//from firebase
   projectId: "malaria-patient-register",
   storageBucket: "malaria-patient-register.appspot.com",
   messagingSenderId: "190350713123",
-  appId: "1:190350713123:web:96a0bf75b9902867526c96",
+  appId: "",
   measurementId: "G-0HECTYZXK3"
 };
 
